772612bac8ba98e6c710be4dc2e5d77ab2986b8a
An1ket Gupta <guptaaniket600.ag@gmail.com>
2026-01-14 14:47:18 +0530

    Bug fixes user mentioned

--- src/app/api/users/[userId]/followers/route.ts
+++ src/app/api/users/[userId]/followers/route.ts
@@ -4,8 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
     req: NextRequest,
-    { params }: { params: { userId: string } }
+    props: { params: Promise<{ userId: string }> }
 ) {
+    const params = await props.params;
     try {
         const userId = params.userId;
 
